b7e84e96e221e5490b1d469e9da7f4c1d61b1cd2
Marcel <marcel@Marcels-MacBook-Pro.local>
2014-12-03 15:26:22 +0100

    Initial Commit

---
+++ FitTests/FitTests.m
@@ -0,0 +1,34 @@
+//
+//  FitTests.m
+//  FitTests
+//
+//  Created by Marcel on 03.12.14.
+//  Copyright (c) 2014 Marcel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FitTests : XCTestCase
+
+@end
+
+@implementation FitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

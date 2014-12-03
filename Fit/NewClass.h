e9733aa6d73d652a420ee0c0e2f77f1e1424e617
Marcel <marcel@Marcels-MacBook-Pro.local>
2014-12-03 16:00:45 +0100

    Changes Added Class

---
+++ Fit/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  Fit
+//
+//  Created by Marcel on 03.12.14.
+//  Copyright (c) 2014 Marcel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewClass : UIViewController
+
+@end
